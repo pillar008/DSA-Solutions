70330c287fdc18c4dd097e5918f42ccecc426961
Neeraj Sharma <84141574+pillar008@users.noreply.github.com>
2026-06-01 20:02:07 +0000

    Sync LeetCode submission - Add Binary (javascript)

---
+++ leetcode-history/problems/add_binary/solution.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function(a, b) {
+    let result = "";
+    let i = a.length - 1;
+    let j = b.length - 1;
+    let carry = 0;
+    
+    while (i >= 0 || j >= 0 || carry > 0) {
+        let sum = carry;
+
+        if (i >= 0) {
+            sum += parseInt(a[i]);
+            i--;
+        }
+        
+        if (j >= 0) {
+            sum += parseInt(b[j]);
+            j--;
+        }
+
+
+        result = (sum % 2) + result
+        
+        carry = Math.floor(sum / 2);
+    }
+
+    return result;
+};

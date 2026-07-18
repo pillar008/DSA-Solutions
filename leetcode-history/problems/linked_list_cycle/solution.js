dda3c54c8a563dbe689729a485452e557a59c6ba
Neeraj Sharma <84141574+pillar008@users.noreply.github.com>
2026-07-18 18:40:56 +0000

    Sync LeetCode submission - Linked List Cycle (javascript)

--- leetcode-history/problems/linked_list_cycle/solution.js
+++ leetcode-history/problems/linked_list_cycle/solution.js
@@ -11,14 +11,14 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    let seenNodes = new Set();
-    let curr = head;
-    while(curr != null){
-        if(seenNodes.has(curr)){
+    let fast = head;
+    let slow = head;
+    while(fast != null && fast.next != null){
+        slow = slow.next;
+        fast = fast.next.next;
+        if(fast == slow){
             return true;
         }
-        seenNodes.add(curr);
-        curr = curr.next;
     }
     return false;
 };

f4a4030262839a2aa8c4b5c22c6f88b3716ea51d
Neeraj Sharma <84141574+pillar008@users.noreply.github.com>
2026-07-27 00:48:11 +0530

    Time: 0 ms (100.00%) | Memory: 56.3 MB (89.00%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -10,38 +10,26 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
-var mergeTwoLists = function(list1, list2) {
-    if(!list1) return list2;
-    if(!list2) return list1;
-    let sentinel = null;
-    if(list1.val < list2.val){
-        sentinel = list1;
-        list1 = list1.next;
-    }
-    else{
-        sentinel = list2;
-        list2 = list2.next;
-    }
-    let start =  sentinel;
+var mergeTwoLists = function(list1, list2) {    
+    let start = new ListNode();
+    let curr = start;
 
     while(list1 != null && list2 != null){
         if(list1.val < list2.val){
-            sentinel.next = list1;
+            curr.next = list1;
             list1 = list1.next;
-            sentinel = sentinel.next;
         }
         else{
-            sentinel.next = list2;
+            curr.next = list2;
             list2 = list2.next;
-            sentinel = sentinel.next;
         }
+        curr = curr.next;
     }
-
     if(!list1){
-        sentinel.next = list2;
+        curr.next = list2;
     }
     if(!list2){
-        sentinel.next = list1;
+        curr.next = list1;
     }
-        return start;
+    return start.next;
 };

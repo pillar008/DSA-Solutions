e70f505997c2253df96d6d74f245e8969e527604
Neeraj Sharma <84141574+pillar008@users.noreply.github.com>
2026-07-28 00:19:20 +0530

    Time: 0 ms (100.00%) | Memory: 57.7 MB (37.46%) - LeetSync

--- 61-rotate-list/rotate-list.js
+++ 61-rotate-list/rotate-list.js
@@ -11,27 +11,29 @@
  * @return {ListNode}
  */
 var rotateRight = function(head, k) {
-if (!head || !head.next || k === 0) {
-        return head;
-    }
+  if(!head || !head.next || k ===0){
+    return head;
+  }
 
-    let point = head;
-    let length = 1;
-    while(head.next != null){
-        length++;
-        head = head.next;
-    }
-    head.next = point;
-    // head = head.next;
-    // let result = head;
+  let point = head;
+  let length = 1;
 
-    let size = k%length;
-    let walk = length - size;
-    
-    for(let i=0;i<walk;i++){
-        head = head.next;
-    }
-    let result = head.next;
-    head.next = null;
-    return result;
+  while(head.next != null){
+    length++;
+    head = head.next;
+  }  
+
+  head.next = point;
+
+  let size = k %length;
+  let walk = length-size;
+
+  for(let i=0;i<walk;i++){
+    head = head.next;
+  }
+
+  let result = head.next;
+  head.next = null;
+
+  return result;
 };

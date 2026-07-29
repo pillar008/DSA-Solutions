ec75757b46ca1ff370c0c7d6a3f55468570a5b28
Neeraj Sharma <84141574+pillar008@users.noreply.github.com>
2026-07-29 23:23:47 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (53.12%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -3,7 +3,17 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    s= s.trim();
-    s = s.split(" ");
-    return s[s.length-1].length;
+    let n = s.length - 1;
+    let len = 0;
+    if(n === 1){
+        return 1;
+    }
+    while(n>= 0 && s[n] === " "){
+        n--;
+    }
+    while(n >= 0 && s[n] != " "){
+        len++;
+        n--;
+    }
+    return len;
 };

d62742f87fdfe13814920899c8a8c97b86431407
Neeraj Sharma <84141574+pillar008@users.noreply.github.com>
2026-07-29 22:32:50 +0530

    Time: 0 ms (100.00%) | Memory: 54.1 MB (28.92%) - LeetSync

---
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+    if(!head || !head.next) return head;
+    let left = head;
+    let right = head.next;
+    left.next = swapPairs(right.next);
+    right.next = left;
+    return right;
+};

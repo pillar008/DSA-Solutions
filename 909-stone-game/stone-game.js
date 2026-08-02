d7448ab9ff52bb585a1e429d84c0a21dcdf1de35
Neeraj Sharma <84141574+pillar008@users.noreply.github.com>
2026-08-02 19:57:24 +0530

    Time: 0 ms (100.00%) | Memory: 53.9 MB (52.53%) - LeetSync

--- 909-stone-game/stone-game.js
+++ 909-stone-game/stone-game.js
@@ -3,28 +3,5 @@
  * @return {boolean}
  */
 var stoneGame = function(piles) {
-    const n = piles.length;
-    const memo = Array.from({ length: n }, () => Array(n).fill(null));
-    
-    function getPlayerDiff(left, right, isAlice) {
-        if (left > right) return 0;
-        
-        if (memo[left][right] !== null) {
-            return memo[left][right];
-        }
-        
-        if (isAlice) {
-            const chooseLeft = piles[left] + getPlayerDiff(left + 1, right, false);
-            const chooseRight = piles[right] + getPlayerDiff(left, right - 1, false);
-            memo[left][right] = Math.max(chooseLeft, chooseRight);
-        } else {
-            const chooseLeft = -piles[left] + getPlayerDiff(left + 1, right, true);
-            const chooseRight = -piles[right] + getPlayerDiff(left, right - 1, true);
-            memo[left][right] = Math.min(chooseLeft, chooseRight);
-        }
-        
-        return memo[left][right];
-    }
-    
-    return getPlayerDiff(0, n - 1, true) > 0;
+    return true;
 };

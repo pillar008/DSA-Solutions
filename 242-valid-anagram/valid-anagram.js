acdccc93e632d0deda35d5e87aadb7c9b67c3706
Neeraj Sharma <84141574+pillar008@users.noreply.github.com>
2026-08-03 23:07:26 +0530

    Time: 30 ms (15.66%) | Memory: 57.4 MB (52.69%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -7,12 +7,5 @@ var isAnagram = function(s, t) {
     s = s.split('').sort().join('');
     t = t.split('').sort().join('');
 
-    if(s.length != t.length) return false;
-
-    for(let i = 0;i<s.length;i++){
-        if(s[i] != t[i]){
-            return false;
-        }
-    }
-    return true;
+    return s===t;
 };

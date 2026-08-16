8dc02e890d3f8fb759deb63527ef08e557c7c636
Neeraj Sharma <84141574+pillar008@users.noreply.github.com>
2026-08-16 16:31:43 +0530

    Time: 8 ms (54.55%) | Memory: 67.2 MB (81.82%) - LeetSync

---
+++ 2156-stone-game-ix/stone-game-ix.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} stones
+ * @return {boolean}
+ */
+var stoneGameIX = function(stones) {
+    const counts = [0, 0, 0];
+    
+    for (const stone of stones) {
+        counts[stone % 3]++;
+    }
+    
+    if (counts[0] % 2 === 0) {
+        return Math.min(counts[1], counts[2]) > 0;
+    }
+    
+    return Math.abs(counts[1] - counts[2]) > 2;
+};

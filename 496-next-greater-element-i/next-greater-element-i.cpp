03010c917a646ee6e82012a4985cdf98cafd8faf
Neeraj Sharma <84141574+pillar008@users.noreply.github.com>
2026-08-18 15:18:11 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (63.32%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,25 +1,28 @@
+#include <unordered_map>
+
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> NGE;
+        vector<int> stack;
         vector<int> ans;
 
-        for (int i : nums1) {
-            bool found = false;
-            bool flag = false;
-            for (int j : nums2) {
-                if (i == j) {
-                    found = true;
-                }
-                if (found == true && j > i) {
-                    ans.push_back(j);
-                    flag = true;
-                    break;
-                }
+        for (int i = nums2.size() - 1; i >= 0; i--) {
+            while (!stack.empty() && stack.back() <= nums2[i]) {
+                stack.pop_back();
             }
-            if(found == true && flag == false){
-                ans.push_back(-1);
+            if (!stack.empty() && nums2[i] < stack.back()) {
+                NGE[nums2[i]] = stack.back();
+            } else {
+                NGE[nums2[i]] = -1;
+            }
+            stack.push_back(nums2[i]);
+        }
+        for (int i : nums1) {
+            if (NGE.contains(i)) {
+                ans.push_back(NGE[i]);
             }
         }
-    return ans;
+        return ans;
     }
 };

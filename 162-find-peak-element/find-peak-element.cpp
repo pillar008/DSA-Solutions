9f8d13e04f1cf83db1024281d08a40036f357caa
Neeraj Sharma <84141574+pillar008@users.noreply.github.com>
2026-09-04 16:52:24 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (79.11%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -4,30 +4,16 @@ public:
         int low = 0;
         int high = nums.size()-1;
 
-        if(nums.size()==1) return 0;
-
-        if(nums[low]>nums[low+1]){
-            return low;
-        }
-        low++;
-        if(nums[high]>nums[high-1]){
-            return high;
-        }
-        high--;
-        
-        while(low<=high){
+        while(low<high){
             int mid = low + (high-low)/2;
 
-            if(nums[mid]> nums[mid+1] && nums[mid] > nums[mid-1]){
-                return mid;
-            }
-            else if(nums[mid] < nums[mid +1]){
-                low = mid+1;
+            if(nums[mid] < nums[mid+1]){
+                low = mid +1;
             }
             else{
-                high = mid-1;
+                high = mid;
             }
         }
-        return 0;
+        return low;
     }
 };
